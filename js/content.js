c32f1fc431ee7307698e03133f96f6f143b0356c
Brandon Espinosa <brandon.eg.yeoman@gmail.com>
2019-12-19 02:08:13 -0600

    Espinosa Arquitectura version 2, added tweaks for content page, added mobile viewport fix, work in progress

--- js/content.js
+++ js/content.js
@@ -5,12 +5,32 @@ const displaySlides = document.querySelectorAll(".content-slide");
 menuOptions.forEach((option, optIndex) => {
   option.addEventListener("mouseover", function() {
     hideContentSlides();
-    displaySlides[optIndex].classList.add("content-slide-active");
+    showContentSlide(option, displaySlides[optIndex]);
   });
 });
 
+function showContentSlide(opt, sld) {
+  sld.classList.add("content-slide-active");
+
+  let isText = sld.classList.contains("content-slide-text");
+  let newTop;
+  let slideHeight = sld.offsetHeight / 2;
+
+  if (isText) {
+    let optHeight = opt.offsetHeight / 2;
+    let optTop = opt.offsetTop + optHeight;
+    newTop = optTop - slideHeight + "px";
+  } else {
+    newTop = "calc(50% - " + slideHeight + "px)";
+  }
+
+  sld.style.top = newTop;
+}
+
 function hideContentSlides() {
   for (let i = 0; i < displaySlides.length; i++) {
+    let prevTop = displaySlides[i].offsetTop;
+    displaySlides[i].style.top = "" + prevTop / 2 + "px";
     displaySlides[i].classList.remove("content-slide-active");
   }
 }
---
+++ js/mobile.js
@@ -0,0 +1,4 @@
+let vh = window.innerHeight * 0.01;
+let vhValue = vh + "px";
+
+document.documentElement.style.setProperty("--vh", vhValue);
