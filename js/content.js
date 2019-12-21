04ffc482934435efe66879f5675b93c3992ac764
Brandon Espinosa <brandon.eg.yeoman@gmail.com>
2019-12-20 23:58:31 -0600

    Test for mobile Content page display

--- js/content.js
+++ js/content.js
@@ -19,25 +19,29 @@ menuOptions.forEach((option, optIndex) => {
 function showContentSlide(opt, sld) {
   sld.classList.add("content-slide-active");
 
-  let isText = sld.classList.contains("content-slide-text");
-  let newTop;
-  let slideHeight = sld.offsetHeight / 2;
+  if (document.body.clientWidth >= 1025) {
+    let isText = sld.classList.contains("content-slide-text");
+    let newTop;
+    let slideHeight = sld.offsetHeight / 2;
 
-  if (isText) {
-    let optHeight = opt.offsetHeight / 2;
-    let optTop = opt.offsetTop + optHeight;
-    newTop = optTop - slideHeight + "px";
-  } else {
-    newTop = "calc(50% - " + slideHeight + "px)";
+    if (isText) {
+      let optHeight = opt.offsetHeight / 2;
+      let optTop = opt.offsetTop + optHeight;
+      newTop = optTop - slideHeight + "px";
+    } else {
+      newTop = "calc(50% - " + slideHeight + "px)";
+    }
+
+    sld.style.top = newTop;
   }
-
-  sld.style.top = newTop;
 }
 
 function hideContentSlides() {
   for (let i = 0; i < displaySlides.length; i++) {
-    let prevTop = displaySlides[i].offsetTop;
-    displaySlides[i].style.top = "" + prevTop / 2 + "px";
+    if (document.body.clientWidth >= 1025) {
+      let prevTop = displaySlides[i].offsetTop;
+      displaySlides[i].style.top = "" + prevTop / 2 + "px";
+    }
     displaySlides[i].classList.remove("content-slide-active");
   }
 }
